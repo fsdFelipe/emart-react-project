6040c420aa354206809d4668ddf73ab99796edf0
fsdFelipe <98924712+fsdFelipe@users.noreply.github.com>
2022-07-07 17:51:54 -0300

    Modificando Navbar

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -1,12 +1,13 @@
+import "./nav.css";
 export default function Navbar() {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-          Navbar
+    <nav className="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
+      <div className="container">
+        <a className="navbar-brand fw-bold" href="#">
+          Minha Loja
         </a>
         <button
-          class="navbar-toggler"
+          className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarSupportedContent"
@@ -14,67 +15,43 @@ export default function Navbar() {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
                 Home
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                Link
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Categorias
               </a>
             </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Dropdown
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Produtos
               </a>
-              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <hr class="dropdown-divider" />
-                </li>
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
             </li>
-            <li class="nav-item">
-              <a class="nav-link disabled">Disabled</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Sobre nós
+              </a>
             </li>
           </ul>
-          <form class="d-flex">
-            <input
-              class="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button class="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
+          <button type="button" className="btn btn-outline-dark">
+            <i className="fa-solid fa-arrow-right-to-bracket me-1"></i>
+            Login
+          </button>
+          <button type="button" className="btn btn-outline-dark ms-2">
+            <i class="fa-solid fa-user-plus me-1"></i>
+            Registrar
+          </button>
+          <button type="button" className="btn btn-outline-dark ms-2">
+            <i class="fa-solid fa-cart-shopping me-1"></i>
+            Carrinho (0)
+          </button>
         </div>
       </div>
     </nav>
